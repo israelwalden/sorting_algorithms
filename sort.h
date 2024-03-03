5aef3819e316c547777a2fa498ddded8846b9b4e
israelwalden <waldenisraelk@gmail.com>
2024-03-03 06:49:39 +0000

    task 0

---
+++ sort.h
@@ -0,0 +1,25 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+void bubble_sort(int *array, size_t size);
+
+
+
+#endif 
